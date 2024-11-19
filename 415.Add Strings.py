58b85b5377e442e4157234bd0d55c862d93e59c8
Habib Faisal <habibfaisaljandira@gmail.com>
2024-11-19 21:57:18 +0500

    Add files via upload

---
+++ 415.Add Strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        inta = int(num1)
+        intb = int(num2)
+        total = inta + intb
+        return str(total)
